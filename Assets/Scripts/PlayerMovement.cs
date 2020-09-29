9e09617c2c6b880ac167aa275f61339e5984682a
joaquinrobles64 <joaquinrobles64@yahoo.com>
2020-09-28 17:27:49 -0700

    player movement and camera follows player

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("PlayerMovement Start()");
+        _speed = 6f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(_speed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, _speed * Input.GetAxis("Vertical") * Time.deltaTime);
+    }
+}
