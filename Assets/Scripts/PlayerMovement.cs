3e7f6f162ec07c3a10de9241cd08d59826130eca
Noah <noah_castro25@yahoo.com>
2020-10-09 17:49:55 -0700

    Things are back to where they are supposed to be
    Deleted Scripts on Accident, brought it bacl

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 6f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xAxis = Input.GetAxis("Horizontal");
+        float zAxis = Input.GetAxis("Vertical");
+        
+        transform.Translate(xAxis * speed * Time.deltaTime, 0f, zAxis * speed * Time.deltaTime);
+    }
+}
