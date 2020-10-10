2627f20a24a927dacc751180b1ac7396abf66f3c
joaquinrobles64 <joaquinrobles64@yahoo.com>
2020-10-09 19:01:43 -0700

    this sucks

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    private float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 10f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Attack();
+        }
+
+    }
+
+    void Movement()
+    {
+        float xAxis = Input.GetAxis("Horizontal");
+        float zAxis = Input.GetAxis("Vertical");
+        transform.Translate(xAxis * speed * Time.deltaTime, 0f, zAxis * speed * Time.deltaTime);
+    }
+
+    void Attack()
+    {
+        Instantiate()
+    }
+}
