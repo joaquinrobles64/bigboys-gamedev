27ba83df8deed7c24985d9d9d5eb9ba4d2218d1e
joaquinrobles64 <joaquinrobles64@yahoo.com>
2020-10-09 21:22:27 -0700

    added asset player model (previous push didn't work??)

---
+++ Assets/ChangeAnimationCowboy.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAnimationCowboy : MonoBehaviour
+{
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            anim.SetTrigger("Attack");
+        }
+
+        if (Input.GetKeyDown(KeyCode.W)){
+            anim.SetTrigger("Run");   
+        }
+
+        if (Input.GetKeyDown(KeyCode.A)){
+            anim.SetTrigger("Run");   
+        }
+
+        if (Input.GetKeyDown(KeyCode.S)){
+            anim.SetTrigger("Run");   
+        }
+
+        if (Input.GetKeyDown(KeyCode.D)){
+            anim.SetTrigger("Run");   
+        }
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            anim.SetTrigger("Idle");
+        }
+
+        // if(Input.GetKeyDown() == null) {
+        //     anim.SetTrigger("Idle");
+        // }
+
+
+    }
+}
---
+++ Assets/Scripts/CowboyController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CowboyController : MonoBehaviour
+{
+    private float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 6f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Attack();
+
+        }
+
+    }
+
+    void Movement()
+    {
+
+        float xAxis = Input.GetAxis("Vertical");
+        float zAxis = Input.GetAxis("Horizontal");
+        transform.Translate(xAxis * speed * Time.deltaTime, 0f, -zAxis * speed * Time.deltaTime);
+    }
+
+    void Attack()
+    {
+        
+    }
+}
