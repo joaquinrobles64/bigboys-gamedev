52e8708b8a1c6a88735cd143273a0bac7ac2f90f
joaquinrobles64 <joaquinrobles64@yahoo.com>
2020-10-09 17:32:39 -0700

    added playercontroller

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    private float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 6f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Attack();
+        }
+        Attack();
+
+    }
+
+    void Movement()
+    {
+        float xAxis = Input.GetAxis("Horizontal");
+        float zAxis = Input.GetAxis("Vertical");
+        transform.Translate(xAxis * speed * Time.deltaTime, 0f, zAxis * speed * Time.deltaTime);
+    }
+
+    void Attack()
+    {
+        
+    }
+}
