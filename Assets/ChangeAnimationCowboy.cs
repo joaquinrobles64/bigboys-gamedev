2e5c5da6d962f6fef2fac053e6206eda1da733ab
Noah <noah_castro25@yahoo.com>
2020-10-09 19:19:50 -0700

    Character, etc
    Added Character Model and Animations from it.

---
+++ Assets/ChangeAnimationCowboy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAnimationCowboy : MonoBehaviour
+{
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            anim.SetTrigger("Attack");
+        }
+
+        if (Input.GetKeyDown(KeyCode.J))
+            anim.SetTrigger("Run");   
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            anim.SetTrigger("Idle");
+        }
+    }
+}
--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -8,6 +8,6 @@ public class Rotator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
     }
 }
