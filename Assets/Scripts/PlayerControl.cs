2742401fdd6b5c8b80c9c8ddf2d630769d7b624c
joaquinrobles64 <joaquinrobles64@yahoo.com>
2020-10-16 00:26:24 -0700

    8 directional movement relative to camera and organized some folders

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -5,34 +5,51 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    private float speed;
+    public float speed;
+    public float turnspeed;
+    private Vector2 input;
+    private float angle;
+    private Quaternion targetRotation;
+    Transform cam;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        speed = 7f;
+        cam = Camera.main.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Movement();
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            Attack();
-        }
+        GetInput();
+        if (Mathf.Abs(input.x) < 1 && Mathf.Abs(input.y) < 1) { return; }
+        CalculateDirection();
+        Rotate();
+        Move();
     }
 
-    void Movement()
+    void GetInput()
     {
-        float xAxis = Input.GetAxis("Horizontal");
-        float zAxis = Input.GetAxis("Vertical");
-        transform.Translate(xAxis * speed * Time.deltaTime, 0f, zAxis * speed * Time.deltaTime);
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
     }
 
-    void Attack()
+    void CalculateDirection()
     {
-        
+        angle = Mathf.Atan2(input.x, input.y);
+        angle = Mathf.Rad2Deg * angle;
+        angle += cam.eulerAngles.y;
+    }
+
+    void Rotate()
+    {
+        targetRotation = Quaternion.Euler(0, angle, 0);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, turnspeed * Time.deltaTime);
+    }
+
+    void Move()
+    {
+        transform.position += transform.forward * speed * Time.deltaTime;
     }
 }
